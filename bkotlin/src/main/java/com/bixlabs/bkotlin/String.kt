1f1b2c548fbcda04f1ff20ea1a2a6538429026a4
Matias <mradzinski@gmail.com>
2018-01-15 20:08:37 -0300

    Added String extension containsExact

--- bkotlin/src/main/java/com/bixlabs/bkotlin/String.kt
+++ bkotlin/src/main/java/com/bixlabs/bkotlin/String.kt
@@ -1,6 +1,7 @@
 package com.bixlabs.bkotlin
 
 import android.util.Patterns
+import java.util.regex.Pattern
 
 /**
  * Short for `foo: String = ""`
@@ -16,3 +17,14 @@ fun String.isNotBlankOrEmpty(): Boolean = !this.isEmpty() || !all { it.isWhitesp
  * Returns true if this string is a valid URL
  */
 fun String.isValidUrl() : Boolean = Patterns.WEB_URL.matcher(this.toLowerCase()).matches()
+
+/**
+ * Returns true if this string contains exactly the provided string.
+ * This method uses RegEx to evaluate and its case-sensitive. What makes it different from the classic
+ * [contains] is that it doesn't uses [indexOf], hence it's more performant when used on long char sequences
+ * and has much higher probabilities of not returning false positives per approximation.
+ */
+fun String.containsExact(string: String): Boolean =
+        Pattern.compile("(?<=^|[^a-zA-Z0-9])\\Q$string\\E(?=\$|[^a-zA-Z0-9])")
+                .matcher(this)
+                .find()
