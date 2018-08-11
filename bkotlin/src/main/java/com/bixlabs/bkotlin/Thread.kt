1af4ecb2c6da3ac1d1d7100be8c752a9d7121406
Matias <mradzinski@gmail.com>
2018-08-11 17:29:32 -0300

    Improved Thread extensions syntax and definitions

--- bkotlin/src/main/java/com/bixlabs/bkotlin/Thread.kt
+++ bkotlin/src/main/java/com/bixlabs/bkotlin/Thread.kt
@@ -7,22 +7,23 @@ import android.os.Looper
 /**
  * Runs the specified action on the UI thread. This method can be invoked from anywhere on the application
  */
-fun runOnUiThread(action: () -> Unit) = Handler(Looper.getMainLooper()).post(Runnable(action))
+inline fun runOnUiThread(crossinline action: () -> Unit) = Handler(Looper.getMainLooper())
+        .post { action.invoke() }
 
 /**
  * Runs the specified action on a background thread with delay.
  * @param[delayInMillis] delay in ms
  */
-fun runDelayed(delayInMillis: Long, callback: () -> Unit) = Handler()
-        .postDelayed(Runnable(callback), delayInMillis)
+inline fun runDelayed(delayInMillis: Long, crossinline callback: () -> Unit) = Handler()
+        .postDelayed({ callback.invoke() }, delayInMillis)
 
 /**
  * Runs the specified action on the UI thread with delay.
  * @param[delayInMillis] The delay in milliseconds
  * @param[callback] The block that will be executed
  */
-fun Context.runDelayedOnUiThread(delayInMillis: Long, callback: () -> Unit) = Handler(this.mainLooper)
-        .postDelayed({ callback() }, delayInMillis)
+inline fun Context.runDelayedOnUiThread(delayInMillis: Long, crossinline callback: () -> Unit) = Handler(this.mainLooper)
+        .postDelayed({ callback.invoke() }, delayInMillis)
 
 /**
  * Safe version of [runDelayedOnUiThread] where [callback] is surrounded by [attempt] to
@@ -30,5 +31,5 @@ fun Context.runDelayedOnUiThread(delayInMillis: Long, callback: () -> Unit) = Ha
  * @param[delayInMillis] The delay in milliseconds
  * @param[callback] The block that will be executed
  */
-fun Context.safeRunDelayedOnUiThread(delayInMillis: Long, callback: () -> Unit) = Handler(this.mainLooper)
-        .postDelayed({ attempt { callback() } }, delayInMillis)
+inline fun Context.safeRunDelayedOnUiThread(delayInMillis: Long, crossinline callback: () -> Unit) = Handler(this.mainLooper)
+        .postDelayed({ attempt { callback.invoke() } }, delayInMillis)
