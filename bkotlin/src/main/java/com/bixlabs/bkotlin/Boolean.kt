9ba269122cdf7a00e6c2f66d67afd780fe295cc7
Matias <mradzinski@gmail.com>
2018-10-15 09:42:17 -0300

    Added two new optional Boolean extensions

--- bkotlin/src/main/java/com/bixlabs/bkotlin/Boolean.kt
+++ bkotlin/src/main/java/com/bixlabs/bkotlin/Boolean.kt
@@ -4,3 +4,15 @@ package com.bixlabs.bkotlin
  * Returns false if this boolean is null
  */
 fun Boolean?.orFalse(): Boolean = this ?: false
+
+/**
+ * Determines if the value of this optional Boolean is not null and equals to `true`
+ * @return `true` if this optonal boolean is not null and `true`, `false` if this Boolean is null or `false`.
+ */
+fun Boolean?.isTrue(): Boolean = if (this == null) false else this == true
+
+/**
+ * Determines if the value of this optional Boolean is not null and equals to `false`
+ * @return `true` if this optonal boolean is not null and `false`, `false` if this Boolean is null or `false`.
+ */
+fun Boolean?.isFalse(): Boolean = if (this == null) false else this == false
