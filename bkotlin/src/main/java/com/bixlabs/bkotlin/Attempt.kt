5d492a33c79ee0a6f657ef83a628970f28861d6d
Matias <mradzinski@gmail.com>
2018-10-18 00:29:48 -0300

    Improved attempt documentation and added result method

--- bkotlin/src/main/java/com/bixlabs/bkotlin/Attempt.kt
+++ bkotlin/src/main/java/com/bixlabs/bkotlin/Attempt.kt
@@ -9,6 +9,11 @@ package com.bixlabs.bkotlin
  */
 @Suppress("MemberVisibilityCanPrivate")
 data class AttemptResult<out T> @PublishedApi internal constructor(val value: T?, val error: Throwable?) {
+
+    /**
+     * Concats a new [attempt], calling [f] with the previous [attempt] result value as its argument
+     * and returns the result of this new [attempt] in the form of an [AttemptResult]
+     */
     @Suppress("UNCHECKED_CAST")
     inline fun <R> then(f: (T) -> R): AttemptResult<R> {
         if (isError) {
@@ -19,23 +24,42 @@ data class AttemptResult<out T> @PublishedApi internal constructor(val value: T?
         return attempt { f(value as T) }
     }
 
+    /**
+     * Calls [f] with the [attempt] result value as its argument and
+     * returns the [attempt] result value (if any)
+     */
+    @Suppress("UNCHECKED_CAST")
+    inline fun result(f: (T?) -> Unit) : T? {
+        f(value as T)
+
+        return this.value
+    }
+
+    /**
+     * `true` if this [attempt] resulted in an error. `false` otherwise.
+     */
     inline val isError: Boolean
         get() = error != null
 
+    /**
+     * `true` if this [attempt] didn't result in an error. `false` otherwise.
+     */
     inline val hasValue: Boolean
         get() = error == null
 }
 
 /**
- * Execute [f] and return the result or an exception, if an exception was occurred.
+ * Executes [f] and return the result or an exception, if an exception was occurred.
  */
 inline fun <T> attempt(f: () -> T): AttemptResult<T> {
     var value: T? = null
     var error: Throwable? = null
+
     try {
         value = f()
     } catch(t: Throwable) {
         error = t
     }
+
     return AttemptResult(value, error)
 }
