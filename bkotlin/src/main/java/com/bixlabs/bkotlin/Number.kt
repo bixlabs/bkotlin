46f4c4cdb41d97619cbdac87d7f937c96d59fd02
Matias <mradzinski@gmail.com>
2019-03-02 17:23:25 -0300

    Added Number times extension

--- bkotlin/src/main/java/com/bixlabs/bkotlin/Number.kt
+++ bkotlin/src/main/java/com/bixlabs/bkotlin/Number.kt
@@ -29,3 +29,10 @@ fun Float?.isNotNullAndMoreThanZero(): Boolean = this != null && this > 0F
  * Returns true if this number is not null or zero (0)
  */
 fun Double?.isNotNullAndMoreThanZero(): Boolean = this != null && this > 0.0
+
+/**
+ * Loops through this number, executing the given function [action] every time.
+ *
+ * A zero-based index of current iteration is passed as a parameter to [action].
+ */
+inline fun Number.times(action: (Int) -> Unit) = repeat(this.toInt(), action)
